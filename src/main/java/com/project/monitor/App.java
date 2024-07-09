f44244792d4969eab29a107cd206f75875b2ab5f
Carlito <carlitopelaez1@gmail.com>
2024-07-09 19:17:19 +0800

    updated

--- src/main/java/com/project/monitor/App.java
+++ src/main/java/com/project/monitor/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
             stage.show();
 
         }catch (Exception e){
-            e.printStackTrace();
+            e.printStackTrace();//asdsadas
             //
 
         }
